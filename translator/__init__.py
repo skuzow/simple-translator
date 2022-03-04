7b7a2c22d1e2c257a2124adc41d387079faa7049
legendnightt <alex@legend.sh>
2022-03-04 07:11:23 +0100

    fix: Proper format

--- translator/__init__.py
+++ translator/__init__.py
@@ -1,7 +1,7 @@
 import os
 from typing import Optional
-from googletrans import Translator
 
+from googletrans import Translator
 from mcdreforged.api.all import *
 
 PLUGIN_METADATA = ServerInterface.get_instance().as_plugin_server_interface().get_self_metadata()
@@ -20,7 +20,8 @@ server_inst: PluginServerInterface
 
 
 def auto_translator(msg: str):
-    return translator.translate(msg, dest=config.secondary_language).text if translator.detect(msg).lang == config.global_language else translator.translate(msg, dest=config.global_language).text
+    return translator.translate(msg, dest=config.secondary_language).text if translator.detect(
+        msg).lang == config.global_language else translator.translate(msg, dest=config.global_language).text
 
 
 def on_user_info(server: ServerInterface, info: Info):
@@ -32,11 +33,13 @@ def on_load(server: PluginServerInterface, old):
     global server_inst
     server_inst = server
     load_config(None)
-    help_message = f'Use t and space, then what you want to translate, {config.global_language.upper()} to {config.secondary_language.upper()} & ALL LANGUAGES to {config.global_language.upper()} auto'
+    help_message = f'Use t and space, then what you want to translate, {config.global_language.upper()} to ' \
+                   f'{config.secondary_language.upper()} & ALL LANGUAGES to {config.global_language.upper()} auto'
     server.register_help_message(prefix, help_message)
 
 
 def load_config(source: Optional[CommandSource]):
     global config, server_inst
     config_file_path = os.path.join('config', '{}.json'.format(PLUGIN_METADATA.id))
-    config = server_inst.load_config_simple(config_file_path, in_data_folder=False, source_to_reply=source, echo_in_console=False, target_class=Config)
+    config = server_inst.load_config_simple(config_file_path, in_data_folder=False, source_to_reply=source,
+                                            echo_in_console=False, target_class=Config)
