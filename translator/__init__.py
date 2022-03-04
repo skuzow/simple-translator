7283d1abfc4cc917aceca36059114afc1542761a
legendnightt <alex@legend.sh>
2022-03-04 04:54:58 +0100

    fix: Global visible translation

--- translator/__init__.py
+++ translator/__init__.py
@@ -1,19 +1,21 @@
 from mcdreforged.api.all import *
 from googletrans import Translator
+
 translator = Translator()
 
-# autoTranslator stuff
-def autoTranslator(msg: str):
+
+def auto_translator(msg: str):
     if translator.detect(msg).lang == 'en':
         return translator.translate(msg, dest='es').text
     else:
         return translator.translate(msg, dest='en').text
 
-# MCDReforged stuff
+
 def on_user_info(server: ServerInterface, info: Info):
     if info.content.startswith('t '):
-        server.reply(info, f'§7[T]<{info.player}> §f{autoTranslator(info.content[2:])}')
+        server.say(f'§7[T]<{info.player}> §f{auto_translator(info.content[2:])}')
 
-def on_load(server: ServerInterface, old_module):
+
+def on_load(server: PluginServerInterface):
     server.register_help_message('Translator (t )',
                                  'Use t and space, then what you want to translate, EN to ES & ALL LANGUAGES to EN auto')
