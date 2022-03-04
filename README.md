a3d4a1c148f72683e32f170a586f2c7a0161046b
legendnightt <alex@legend.sh>
2022-03-04 06:19:47 +0100

    new: Config \o/

--- README.md
+++ README.md
@@ -1,10 +1,10 @@
-# MCDR-Translator
+# mcdr-translator
 
 [![license](https://img.shields.io/github/license/legendnightt/MCDR-Translator.svg)](https://github.com/legendnightt/MCDR-Translator/blob/master/LICENSE)
 [![plugin package build status](https://github.com/legendnightt/MCDR-Translator/actions/workflows/package.yml/badge.svg?branch=master)](https://github.com/legendnightt/MCDR-Translator/actions/workflows/package.yml)
 [![supported python versions](https://img.shields.io/badge/python->=%203.6%20-blue)](https://www.python.org/downloads)
 
-Really simple [MCDReforged](https://github.com/Fallen-Breath/MCDReforged) plugin for translating text ingame, from all languages to English, and English to Spanish automatically.
+Really simple [MCDReforged](https://github.com/Fallen-Breath/MCDReforged) plugin for translating text ingame, from all languages to `global_language`, and `secondary_language` to `global_language` automatically.
 
 More plugins in [MCDReforgedPluginsCatalogue](https://github.com/MCDReforged/PluginCatalogue/blob/catalogue/readme.md).
 
@@ -18,14 +18,25 @@ Simply type `t what you want to translate`.
 
 ## Required python modules
 
-- googletrans==4.0.0rc1
-- mcdreforged
+- [googletrans](https://pypi.org/project/googletrans/4.0.0rc1) == 4.0.0rc1
+- [mcdreforged](https://github.com/Fallen-Breath/MCDReforged) >= 1.2.0
 
 To install them execute:
 ```bash
 pip install googletrans==4.0.0rc1 mcdreforged
 ```
-Or download requirements.txt and execute:
+Or download [requirements.txt](https://github.com/legendnightt/mcdr-translator/blob/master/requirements.txt), and execute:
 ```bash
 pip install -r requirements.txt
+```
+
+## Config
+
+`config/translator.json`
+
+```json
+{
+    "global_language": "en",
+    "secondary_language": "es"
+}
 ```
--- translator/__init__.py
+++ translator/__init__.py
@@ -1,14 +1,26 @@
-from mcdreforged.api.all import *
+import os
+from typing import Optional
 from googletrans import Translator
 
+from mcdreforged.api.all import *
+
+PLUGIN_METADATA = ServerInterface.get_instance().as_plugin_server_interface().get_self_metadata()
+
+
+class Config(Serializable):
+    global_language: str = 'en'
+    secondary_language: str = 'es'
+
+
+config: Optional[Config] = None
 translator = Translator()
+prefix = 'Translator (t )'
+plugin_name = PLUGIN_METADATA.name
+server_inst: PluginServerInterface
 
 
 def auto_translator(msg: str):
-    if translator.detect(msg).lang == 'en':
-        return translator.translate(msg, dest='es').text
-    else:
-        return translator.translate(msg, dest='en').text
+    return translator.translate(msg, dest=config.secondary_language).text if translator.detect(msg).lang == config.global_language else translator.translate(msg, dest=config.global_language).text
 
 
 def on_user_info(server: ServerInterface, info: Info):
@@ -16,6 +28,15 @@ def on_user_info(server: ServerInterface, info: Info):
         server.say(f'§7[T]<{info.player}> §f{auto_translator(info.content[2:])}')
 
 
-def on_load(server: PluginServerInterface):
-    server.register_help_message('Translator (t )',
-                                 'Use t and space, then what you want to translate, EN to ES & ALL LANGUAGES to EN auto')
+def on_load(server: PluginServerInterface, old):
+    global server_inst
+    server_inst = server
+    load_config(None)
+    help_message = f'Use t and space, then what you want to translate, {config.global_language.upper()} to {config.secondary_language.upper()} & ALL LANGUAGES to {config.global_language.upper()} auto'
+    server.register_help_message(prefix, help_message)
+
+
+def load_config(source: Optional[CommandSource]):
+    global config, server_inst
+    config_file_path = os.path.join('config', '{}.json'.format(PLUGIN_METADATA.id))
+    config = server_inst.load_config_simple(config_file_path, in_data_folder=False, source_to_reply=source, echo_in_console=False, target_class=Config)
