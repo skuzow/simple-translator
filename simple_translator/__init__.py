3f4a81b06c9efcd95fc25822d1a4ed43e044253a
skuzow <alex@skuzow.net>
2022-07-02 22:06:56 +0200

    refactor: plugin name change

--- simple_translator/__init__.py
+++ simple_translator/__init__.py
@@ -4,7 +4,7 @@ from typing import Optional
 from googletrans import Translator
 from mcdreforged.api.all import *
 
-PLUGIN_METADATA = ServerInterface.get_instance().as_plugin_server_interface().get_self_metadata()
+plugin_metadata = ServerInterface.get_instance().as_plugin_server_interface().get_self_metadata()
 
 
 class Config(Serializable):
@@ -13,15 +13,13 @@ class Config(Serializable):
 
 
 config: Optional[Config] = None
-translator = Translator()
-prefix = 'Translator (t )'
-plugin_name = PLUGIN_METADATA.name
 server_inst: PluginServerInterface
+translator = Translator()
 
 
-def auto_translator(msg: str):
-    return translator.translate(msg, dest=config.secondary_language).text if translator.detect(
-        msg).lang == config.global_language else translator.translate(msg, dest=config.global_language).text
+def auto_translator(message: str):
+    return translator.translate(message, dest=config.secondary_language).text if translator.detect(message).\
+           lang == config.global_language else translator.translate(message, dest=config.global_language).text
 
 
 def on_user_info(server: ServerInterface, info: Info):
@@ -33,13 +31,14 @@ def on_load(server: PluginServerInterface, old):
     global server_inst
     server_inst = server
     load_config(None)
-    help_message = f'Use t and space, then what you want to translate, {config.global_language.upper()} to ' \
-                   f'{config.secondary_language.upper()} & ALL LANGUAGES to {config.global_language.upper()} auto'
+    prefix = 't translate-text'
+    help_message = f'Use t and space, then what you want to translate, works from {config.global_language} to ' \
+                   f'{config.secondary_language} & all languages to {config.global_language}'
     server.register_help_message(prefix, help_message)
 
 
 def load_config(source: Optional[CommandSource]):
     global config, server_inst
-    config_file_path = os.path.join('config', '{}.json'.format(PLUGIN_METADATA.id))
+    config_file_path = os.path.join('config', '{}.json'.format(plugin_metadata.id))
     config = server_inst.load_config_simple(config_file_path, in_data_folder=False, source_to_reply=source,
                                             echo_in_console=False, target_class=Config)
